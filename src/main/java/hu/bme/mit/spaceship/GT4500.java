dac834b19b0e5caba677b6be96e7acde347d44b6
meres <laszlo.dani@gmail.com>
2018-05-04 00:00:41 -0700

    TODO delete

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -16,7 +16,7 @@ public class GT4500 implements SpaceShip {
   }
 
   public boolean fireLaser(FiringMode firingMode) {
-    // TODO not implemented yet
+    // oh
     return false;
   }
 
@@ -78,7 +78,6 @@ public class GT4500 implements SpaceShip {
 
       case ALL:
         // try to fire both of the torpedo stores
-        //TODO implement feature
         if(! primaryTorpedoStore.isEmpty()){
           firingSuccess = primaryTorpedoStore.fire(1);
           wasPrimaryFiredLast = true;
