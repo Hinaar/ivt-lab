9be1fd0ed5d0c9ab15d93c19d97663ccc354f203
Hinaar <laszlo.dani@gmail.com>
2018-05-17 23:52:04 -0700

    commmmiitit

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -9,15 +9,25 @@ import org.junit.Test;
 public class GT4500Test {
 
   private GT4500 ship;
+  private TorpedoStore mockStorePrimary;
+  private TorpedoStore mockStoreSecondary;
 
   @Before
   public void init(){
-    this.ship = new GT4500();
+    mockStorePrimary = mock(TorpedoStore.class);
+    mockStoreSecondary = mock(TorpedoStore.class);
+
+    this.ship = new GT4500(mockStorePrimary, mockStoreSecondary);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
+    when(mockStorePrimary.isEmpty().thenReturn(false);
+    when(mockStorePrimary.fire(1)).thenReturn(true);
+
+    when(mockStoreSecondary.isEmpty()).thenReturn(false);
+    when(mockStoreSecondary.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
